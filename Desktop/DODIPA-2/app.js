637a8800fbe73f08c188d3f4bbfafef51c4ba471
souleymane kone <desolo74@gmail.com>
2020-11-02 09:45:26 -0800

    final changed

--- Desktop/DODIPA-2/app.js
+++ Desktop/DODIPA-2/app.js
@@ -1,49 +1,51 @@
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random/3')
+function getDogImage(number) {
+  fetch('https://dog.ceo/api/breeds/image/random/'+ number)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
+      .catch(error => alert('Something went wrong. Try again later.'));
+      
   
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  //return responseJson.map(response=> displayResults(response)).join('');
+  $('#imgView').empty()
   //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img"> `
-  )
+  for (let i=0; i<responseJson.message.length; i++){
+   $('#imgView').append(
+     `
+    <img src="${responseJson.message[i]}" class="results-img">
+     
+     `  
+   )
+  
+  }
+
   //display the results section
   $('.results').removeClass('hidden');
-}
-function generateThreeImages(messages){
-console.log('hello world');
-let messageArray = [];
-let indexArray = [];
-  messages.forEach(message => {
-  messageArray.push(message);
-  indexArray.push(messages.indexOf(response));
-}); 
-
-return messageArray.map(message => generateThreeImages(message)).join('');
 
 }
 
-
-
+/**retrutn between 1-50 */
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
-    generateThreeImages();
+     let numberDogs = $('.enter').val();
+     $('.enter').val("");
+    if(numberDogs >=1 && numberDogs<=50){
+      getDogImage(numberDogs);
+    }else{
+      alert('Enter Number Between 1-50')
+    };
+    
   });
 }
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
-  generateThreeImages();
   
 });
